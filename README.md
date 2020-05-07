0043fc7195be4592c9a07821d68c0a950154c97f
Dan Weikart <danmweikart@gmail.com>
2020-05-07 15:47:27 -0700

    Started README.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Employee_Roster
+
+I created a command-line application that will create an employee team roster HTML page. 
+
+GIF HERE!!!!
+
+## Technologies Used
+
+This application was created using a combination of node, JavaScript, HTML, and CSS. 
+* Node: requires inquirer, fs, and path
+* JavaScript: Using object oriented programming, an employee class was created and sub-classes for each employee type
+* Application tests were supplied and run for each subclass to ensure functionality
+* Prompts were written using inquirer to allow the user to specify their employee team information
+* User responses are stored in the corresponding sub-classes and those objects are pushed into an array
+* fs writeFile function is used to send the array data to another function which parses the array into the corresponding sub-class objects
+* The data for each sub-class is rendered on an HTML template using template literals
+* These templates are joined into one HTML page and styled using CSS
+
+## Description
--- app.js
+++ app.js
@@ -10,27 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-
-
-// Need to:
-// Fix columns
-// get text to cover black bar
-// Actually replace html in main, link css.
-
-
-
-
-
-
-
-
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
 const newEmployee = [
     {
         type: "list",
@@ -112,18 +91,17 @@ const newIntern = [
 
     }
 ]
-
+//start prompts with manager
 addManager();
 
 function addManager() {
     inquirer.prompt(newManager).then(function(data) {
-        // console.log(data.name);
         let manager = new Manager(data.name, data.ID, data.email, data.officeNumber)
         employees.push(manager);
         addTeamMember();
     })
 }
-
+//user can choose to add new employee (engineer or intern) or finish if they have added all employees
 function addTeamMember () {
     inquirer.prompt(newEmployee).then(function(data) {
         console.log(data.employeeType);
@@ -134,8 +112,6 @@ function addTeamMember () {
             addIntern();
         }
         else {
-            console.log(employees);
-            // render(employees);
             writeToFile(outputPath, render(employees));
         }
     })
@@ -143,7 +119,6 @@ function addTeamMember () {
 
 function addEngineer () {
     inquirer.prompt(newEngineer).then(function(data) {
-        // console.log(data);
         let engineer = new Engineer(data.name, data.ID, data.email, data.github)
         employees.push(engineer);
         addTeamMember();
@@ -152,32 +127,14 @@ function addEngineer () {
 
 function addIntern () {
     inquirer.prompt(newIntern).then(function(data) {
-        // console.log(data);
         let intern = new Intern(data.name, data.ID, data.email, data.school)
         employees.push(intern);
         addTeamMember();
     })
 }
-
-//ask for manager
-//new employee
-// which type
-// create instance
-// output to array
-
-
+// Empty array we will push employee objects into
 const employees = [];
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
       if (err) {
@@ -187,13 +144,3 @@ function writeToFile(fileName, data) {
     });
   }
 
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
